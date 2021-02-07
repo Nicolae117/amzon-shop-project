966e5ace22bf0a202b7f76cd98e1300fc8ea56f1
Nicolae Pop <76559047+NicolaePop117@users.noreply.github.com>
2021-02-07 07:21:25 +0200

    25 Done

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,7 +5,10 @@ import {
   productDetailsReducer,
   productListReducer,
 } from './reducers/productReducers';
-import { userSigninReducer } from './reducers/userReducer';
+import { 
+  userRegisterReducer, 
+  userSigninReducer 
+} from './reducers/userReducer';
 
 const initialState = {
   userSignin: {
@@ -17,7 +20,13 @@ const initialState = {
   cart:{
     cartItems: localStorage.getItem('cartItems')
     ?JSON.parse(localStorage.getItem('cartItems'))
-    :[]
+    :[],
+
+    shippingAddress: localStorage.getItem('shippingAddress')
+      ? JSON.parse(localStorage.getItem('shippingAddress'))
+      : {},
+
+    paymentMethod: 'PayPal',
   }
 };
 
@@ -25,7 +34,8 @@ const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
-  userSignin: userSigninReducer
+  userSignin: userSigninReducer,
+  userRegister: userRegisterReducer,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
