4f48cabb28d495770aa0d5ec36eb0b3fa358b1b4
Nicolae Pop <76559047+NicolaePop117@users.noreply.github.com>
2021-02-07 19:24:12 +0200

    28 done

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,7 +1,8 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_PAYMENT_METHOD, CART_SAVE_SHIPPING_ADDRESS } from "../constants/cartConstant";
+import { CART_ADD_ITEM, CART_EMPTY, CART_REMOVE_ITEM, CART_SAVE_PAYMENT_METHOD, CART_SAVE_SHIPPING_ADDRESS } from "../constants/cartConstant";
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
     switch (action.type) {
+
         case CART_ADD_ITEM:
             const item = action.payload;
             const existItem = state.cartItems.find(x => x.product === item.product)
@@ -16,15 +17,22 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
                     cartItems: [...state.cartItems, item]
                 };
             }
+
         case CART_REMOVE_ITEM:
             return {
                 ...state,
                 cartItems: state.cartItems.filter((x) => x.product !== action.payload),
             };
-            case CART_SAVE_SHIPPING_ADDRESS:
-                return { ...state, shippingAddress: action.payload };
+
+        case CART_SAVE_SHIPPING_ADDRESS:
+            return { ...state, shippingAddress: action.payload };
+        
         case CART_SAVE_PAYMENT_METHOD:
             return { ...state, paymentMethod: action.payload };
+        
+        case CART_EMPTY:
+            return { ...state, cartItems: [] };
+        
         default:
             return state;
     }
